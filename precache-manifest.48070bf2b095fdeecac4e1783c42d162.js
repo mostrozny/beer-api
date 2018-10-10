922482af7c9772ae35017f570a11e531f8acb34e
mostrozny <ostrozny.michal@gmail.com>
2018-10-10 16:49:12 +0100

    Updates

---
+++ precache-manifest.48070bf2b095fdeecac4e1783c42d162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34a75a33117aeae6240f",
+    "url": "/beer-api/static/js/runtime~main.34a75a33.js"
+  },
+  {
+    "revision": "cf8119d1b547647ccae5",
+    "url": "/beer-api/static/js/main.cf8119d1.chunk.js"
+  },
+  {
+    "revision": "47944220611033c4ea5a",
+    "url": "/beer-api/static/js/1.47944220.chunk.js"
+  },
+  {
+    "revision": "cf8119d1b547647ccae5",
+    "url": "/beer-api/static/css/main.48015c13.chunk.css"
+  },
+  {
+    "revision": "e7ffcd30649bc7cfa1cfad51310048a7",
+    "url": "/beer-api/index.html"
+  }
+];
