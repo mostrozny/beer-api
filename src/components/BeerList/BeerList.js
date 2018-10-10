b60b7f2b8805b1fb695686403b09fee6dee0ad06
mostrozny <ostrozny.michal@gmail.com>
2018-10-10 16:12:40 +0100

    end

--- src/components/BeerList/BeerList.js
+++ src/components/BeerList/BeerList.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './BeerList.css';
-import Redirect from "react-router-dom/es/Redirect";
 
 class BeerList extends Component {
     constructor(props) {
@@ -12,7 +11,6 @@ class BeerList extends Component {
          beers: [],
         }
     }
-
 
     getBeers() {
         fetch(`https://api.punkapi.com/v2/beers?page=${this.state.page}&per_page=${this.state.per_page}`)
@@ -35,7 +33,6 @@ class BeerList extends Component {
     }
 
   handleClickItem = (id) => {
-       // return <Redirect to="/BeerSpecs"/>
       this.props.history.push(`/BeerSpecs/${id}`);
   }
 
--- src/components/BeerSpecs/BeerSpecs.js
+++ src/components/BeerSpecs/BeerSpecs.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './BeerSpecs.css';
 
 class BeerSpecs extends Component {
     constructor(props) {
         super(props);
 
-
-
         this.state = {
             id: 0,
             beers: '',
         }
     }
-
 
     setId() {
         const id = this.props.match.params.id;
@@ -21,60 +18,53 @@ class BeerSpecs extends Component {
         }, this.getBeer);
     }
 
-    componentWillMount () {
+    componentWillMount() {
         this.setId();
-    }
-
-    componentDidMount () {
-
     }
 
     getBeer() {
         fetch(`https://api.punkapi.com/v2/beers/${this.state.id}`)
-            .then( response => response.json() )
-            .then( arr => {
+            .then(response => response.json())
+            .then(arr => {
                 this.setState({
                     beers: arr[0],
                 });
             })
     }
 
-  render() {
+    render() {
         const beer = this.state.beers;
-      const food = JSON.stringify(beer.food_pairing);
-        console.log(beer)
-       return (
-          <div className="beerSpecs">
-              <div className="image">
-                  <img src={beer.image_url} />
-              </div>
 
-              <div className="content">
-                <div>
-                    <h2>{beer.name}</h2>
-                    <span className="small">{beer.tagline}</span>
-                    <hr />
+        return (
+            <div className="beerSpecs">
+                <div className="image">
+                    <img src={beer.image_url}/>
                 </div>
-                <div>
-                    <div className="voltage">
-                        <span><b>IBU:</b> {beer.ibu}</span>
-                        <span><b>ABV:</b> {beer.abv}</span>
-                        <span><b>EBC:</b> {beer.ebc}</span>
+
+                <div className="content">
+                    <div>
+                        <h2>{beer.name}</h2>
+                        <span className="small">{beer.tagline}</span>
+                        <hr/>
+                    </div>
+                    <div>
+                        <div className="voltage">
+                            <span><b>IBU:</b> {beer.ibu}</span>
+                            <span><b>ABV:</b> {beer.abv}</span>
+                            <span><b>EBC:</b> {beer.ebc}</span>
+                        </div>
+                    </div>
+                    <div className="description">
+                        {beer.description}
+                    </div>
+                    <div className="served">
+                        <p>Best served with:</p>
+                        {beer.food_pairing}
                     </div>
                 </div>
-                <div className="description">
-                    {beer.description}
-                </div>
-                <div className="served">
-                    <p>Best served with:</p>
-                    {}
-                </div>
-              </div>
-          </div>
-       );
-  }
-
-
+            </div>
+        );
+    }
 }
 
 export default BeerSpecs;
